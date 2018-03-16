bfc9b4595f78d3d50b14d9ce91b7575f395fc288
jacr6 <jose.cuevas.cv@gmail.com>
2018-03-16 16:53:42 -0500

    Update anydesk.bat

--- anydesk.bat
+++ anydesk.bat
@@ -1,4 +1,4 @@
 @echo off
-start /wait anydesk --install"c:\anydesk" --start-with-win --silent --create-shortcuts --create-desktop-icon
+start /wait anydesk.msi --install"c:\anydesk" --start-with-win --silent --create-shortcuts --create-desktop-icon
 echo licence_key | anydesk.exe --register-licence
-echo password | anydesk --set-password
+echo 1qazxsw2 | anydesk --set-password
